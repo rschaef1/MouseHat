9683473b19f202c5726b13f5314b3989b54c7827
rschaef1 <38115439+rschaef1@users.noreply.github.com>
2022-05-03 16:36:06 -0500

    Add files via upload

---
+++ 3transconfig/MPLabProjects/MouseBrains8_3transistor/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# NewConfiguration configuration
+CND_ARTIFACT_DIR_NewConfiguration=dist/NewConfiguration/production
+CND_ARTIFACT_NAME_NewConfiguration=MouseBrains8_3transistor.X.production.hex
+CND_ARTIFACT_PATH_NewConfiguration=dist/NewConfiguration/production/MouseBrains8_3transistor.X.production.hex
+CND_PACKAGE_DIR_NewConfiguration=${CND_DISTDIR}/NewConfiguration/package
+CND_PACKAGE_NAME_NewConfiguration=mousebrains83transistor.x.tar
+CND_PACKAGE_PATH_NewConfiguration=${CND_DISTDIR}/NewConfiguration/package/mousebrains83transistor.x.tar
