844feb19886807f8455f1504b0e1babad1044439
rschaef1 <38115439+rschaef1@users.noreply.github.com>
2022-05-03 16:35:24 -0500

    Add files via upload

---
+++ 3transconfig/MPLabProjects/MouseBrains8_3transistor/build/NewConfiguration/debug/mcc_generated_files/ext_int.p1.d
@@ -0,0 +1,4 @@
+build/NewConfiguration/debug/mcc_generated_files/ext_int.p1:  \
+mcc_generated_files/ext_int.c  \
+mcc_generated_files/ext_int.h  \
+mcc_generated_files/../IR_receiver.h 
---
+++ 3transconfig/MPLabProjects/MouseBrains8_3transistor/build/NewConfiguration/production/tmr1.p1.d
@@ -0,0 +1,4 @@
+build/NewConfiguration/production/tmr1.p1:  \
+tmr1.c  \
+tmr1.h  \
+C:/Users/r.schaefer/Downloads/pic-ir-rx-master/pic-ir-rx-master/MouseBrains8.X/mcc_generated_files/dac.h 
